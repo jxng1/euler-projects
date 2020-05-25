47cac494acbe96bdafb8317471d6dae214d00c5c
Jia Xuan Ng <58788429+jxng1@users.noreply.github.com>
2020-05-25 12:43:06 +0100

    Add files via upload

---
+++ Problem 41/Problem 41/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            int size = 7654321;
+            bool[] primeSieve = new bool[size + 1];
+
+            for (int i = 2; i <= size; i++)
+            {
+                primeSieve[i] = true;
+            }
+
+            for (int i = 2; i <= size; i++)
+            {
+                if (primeSieve[i] == true)
+                {
+                    for (int j = i * 2; j <= size; j += i)
+                    {
+                        primeSieve[j] = false;
+                    }
+                }
+            }
+
+            for (int i = size; i >= 2; i--)
+            {
+                if (primeSieve[i] == true && IsPandigital(i.ToString()) == true) { Console.WriteLine(i); break; }
+            }
+
+            sw.Stop();
+            Console.WriteLine("Time taken: {0}ms", sw.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+        private static bool IsPandigital(string num)
+        {
+            for (int i = 1; i <= num.Length; i++)
+            {
+                if (!num.Contains(Convert.ToString(i))) { return false; }
+            }
+            return true;
+        }
+
+    }
+}
